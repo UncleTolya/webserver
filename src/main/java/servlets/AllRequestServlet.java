1c5a9c90a562ac1a524fbf46e89aa64b1d24c3d2
uncletolya <stolikzakazan@gmail.com>
2019-03-18 23:12:06 +0300

    templater

---
+++ src/main/java/servlets/AllRequestServlet.java
@@ -0,0 +1,54 @@
+package servlets;
+
+import templater.PageGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AllRequestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, Object> pageVariables = createPageVariablesMap(req);
+        pageVariables.put("message", "");
+
+        resp.getWriter().println(PageGenerator.instance().getPage("page.html", pageVariables));
+
+        resp.setContentType("text/html;charset=utf-8");
+        resp.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, Object> pageVariables = createPageVariablesMap(req);
+
+        String message = req.getParameter("message");
+
+        resp.setContentType("text/html;charset=utf-8");
+
+        if (message == null || message.isEmpty()) {
+            resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        } else {
+            resp.setStatus(HttpServletResponse.SC_OK);
+        }
+        pageVariables.put("message", message == null ? "" : message);
+
+        resp.getWriter().println(PageGenerator.instance().getPage("page.html", pageVariables));
+
+    }
+
+    private static Map<String, Object> createPageVariablesMap(HttpServletRequest req) {
+        Map<String, Object> pageVariables = new HashMap<>();
+        pageVariables.put("method", req.getMethod());
+        pageVariables.put("URL", req.getRequestURI());
+        pageVariables.put("pathInfo", req.getPathInfo());
+        pageVariables.put("sessionId", req.getSession().getId());
+        pageVariables.put("parameters", req.getParameterMap().toString());
+        return pageVariables;
+    }
+}
---
+++ src/main/java/templater/PageGenerator.java
@@ -0,0 +1,43 @@
+package templater;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import javax.xml.transform.Templates;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Map;
+
+public class PageGenerator {
+    private static final String HTML_DIR = "templates";
+
+    private static PageGenerator pageGenerator;
+    private final Configuration cfg;
+
+    public static PageGenerator instance() {
+        if (pageGenerator == null) {
+            pageGenerator = new PageGenerator();
+        }
+        return pageGenerator;
+    }
+
+    public String getPage(String fileName, Map<String, Object> data) {
+        Writer stream = new StringWriter();
+        try {
+            Template template = cfg.getTemplate(HTML_DIR + File.separator + fileName);
+            template.process(data, stream);
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stream.toString();
+    }
+
+    private PageGenerator() {
+        cfg = new Configuration();
+    }
+}
